1e683ad5f9cde255219a4ca480f36ef68b2ce85b
TommySaunders84 <100166058+TommySaunders84@users.noreply.github.com>
2025-07-02 05:40:10 -0500

    Add core configuration files

---
+++ next.config.js
@@ -0,0 +1,8 @@
+const withMarkdoc = require('@markdoc/next.js');
+
+module.exports =
+  withMarkdoc(/* config: https://markdoc.io/docs/nextjs#options */)({
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdoc'],
+    reactStrictMode: true,
+    swcMinify: true,
+  });
